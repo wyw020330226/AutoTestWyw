a9b9cb5f92be2dedde1d492e0d38b68f5f5e03d8
Yuanwen WU <wuyw@fenqi.im>
2018-06-07 19:03:45 +0800

    添加testngdemo

---
+++ src/main/java/charter5/BasicAnnotationTest.java
@@ -0,0 +1,14 @@
+package charter5;
+
+import org.testng.annotations.Test;
+
+public class BasicAnnotationTest
+{   
+	@Test
+	public void testCase1()
+	{
+		System.out.println("这是第一个testng测试用例");
+		
+	}
+
+}
