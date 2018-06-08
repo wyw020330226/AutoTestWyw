2bbf08fbcfdeb988b0161781c4ca8c6d061e047f
Yuanwen WU <wuyw@fenqi.im>
2018-06-08 18:36:25 +0800

    添加httplicent测试

--- src/main/java/charter9/MyCookiesForPost.java
+++ src/main/java/charter9/MyCookiesForPost.java
@@ -8,13 +8,16 @@ import java.util.ResourceBundle;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.cookie.Cookie;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class MyCookiesForGet
+public class MyCookiesForPost
 {
 	private ResourceBundle bundle;
 	private String url;
@@ -59,22 +62,34 @@ public class MyCookiesForGet
 	{ "testGetCookies" })
 	public void testGetWithCookies() throws IOException
 	{
-		String gettestwithcookieurl = bundle.getString("testwithcookie.url");
+		String gettestwithcookieurl = bundle.getString("testpostwithcookie");
 		String testurl = url + gettestwithcookieurl;
 
 		// 测试代码逻辑书写
-		HttpGet get = new HttpGet(testurl);
+		HttpPost post = new HttpPost(testurl);
+		JSONObject parm=new JSONObject();
+		parm.put("name", "test");
+		parm.put("age", "20");
+		//设置头信息
+		post.setHeader("content-type","application/json");
+		//将参数添加到方法中
+		StringEntity entity=new StringEntity(parm.toString(),"utf-8");
+		post.setEntity(entity);
+		
 		DefaultHttpClient client = new DefaultHttpClient();
+		//添加cookie
 		client.setCookieStore(cookiestore);
-		HttpResponse response = client.execute(get);
+		HttpResponse response = client.execute(post);
 
-		// 获取响应状态码
-		int statuesCode = response.getStatusLine().getStatusCode();
-		if (200 == statuesCode)
-		{
 			result = EntityUtils.toString(response.getEntity(), "utf-8");
 			System.out.println(result);
-		}
+			
+			JSONObject json=new JSONObject(result);
+			
+			String name=json.getString("name");
+			String age=json.getString("age");
+			System.out.println("name: "+name+" age"+age);
+		
 	}
 
 }
